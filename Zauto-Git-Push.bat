dea85f9d1f67a59be56fe91e9f3f07674a143c86
wingxyq <wingxyq@gmail.com>
2024-03-24 21:59:38 +0800

    自动提交

--- Zauto-Git-Push.bat
+++ Zauto-Git-Push.bat
@@ -1,19 +1,21 @@
 @echo off
-echo Executing Git commands...
+chcp 65001 > nul
+echo 正在执行 Git 命令...
 
 REM 添加所有更改
-echo 添加所有更改...
+echo 正在添加所有更改...
 git add .
 timeout /t 3
 
 REM 提交更改
-echo 提交更改...
-git commit -m "Auto commit"
+echo 正在提交更改...
+git commit -m "自动提交"
 timeout /t 3
 
 REM 推送到远程仓库
-echo 推送到远程仓库...
+echo 正在推送更改到远程仓库...
 git push
+timeout /t 3
 
-echo 执行成功.
+echo Git 命令执行成功。
 pause
