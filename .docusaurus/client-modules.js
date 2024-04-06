85a01a5a59bfadc79d9fa9a009829a2488768863
wingxyq <wingxyq@gmail.com>
2024-04-06 22:30:24 +0800

    df

--- .docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require("C:\\Users\\wingxu\\Desktop\\frankelinli.github.io\\node_modules\\@docusaurus\\plugin-google-gtag\\lib\\gtag"),
-  require("C:\\Users\\wingxu\\Desktop\\frankelinli.github.io\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\Users\\wingxu\\Desktop\\frankelinli.github.io\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("C:\\Users\\wingxu\\Desktop\\frankelinli.github.io\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("C:\\Users\\wingxu\\Desktop\\frankelinli.github.io\\src\\css\\custom.css"),
-  require("C:\\Users\\wingxu\\Desktop\\frankelinli.github.io\\node_modules\\docusaurus-plugin-image-zoom\\lib\\zoom"),
-];
