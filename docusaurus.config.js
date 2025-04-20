bf27297c62857cef3c97432dd91ede99f78ba283
wingxyq <wingxyq@gmail.com>
2025-04-20 21:32:30 +0800

    dfasfdf

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,10 +17,10 @@ const config = {
   favicon: '/img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://haoyelaiga.com',
+  url: 'https://frankelinli.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docs/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
